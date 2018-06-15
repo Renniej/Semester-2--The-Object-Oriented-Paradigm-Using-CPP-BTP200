d1e4e49c0c12768b1c9a14e16687eb26cd76bb1a
tai <tai.juan2@gmail.com>
2018-06-15 11:30:12 -0400

    Finished Code of WS5_Lab, no debugging yet
    Code for WS05 In_lab  finished, needs debugging now

--- Workshops/WS05/in-lab/Fraction.cpp
+++ Workshops/WS05/in-lab/Fraction.cpp
@@ -1,36 +1,160 @@
 // TODO: insert header files
-
+#include <iostream>
+#include "Fraction.h"
 // TODO: continue the namespace
 
+using namespace std;
+namespace sict {
 	// TODO: implement the default constructor
+	Fraction::Fraction() {
+		//safe empty state variables
+		m_numer = 0;
+		m_denom = 0;
 
+	}
 	// TODO: implement the two-argument constructor
+	Fraction::Fraction(int numer, int denom) {
+		
+		if (numer >= 0 && denom > 0) { //check if parameters are valid
+			
+			//Set values from parameter to object variables
+			m_numer = numer;
+			m_denom = denom;
+		} 
+		else { //else set object to empty sate
+			m_numer = 0;
+			m_denom = 0;
+		}
+
+	}
 
 	// TODO: implement the max query
 
-    // TODO: implement the min query
+	int Fraction::max() const {
+
+		int biggest;
+
+		if (m_numer > m_denom){
+
+			biggest = m_numer;
+
+
+			}
+		else {
+			biggest = m_denom;
+		}
+
+		return biggest;
+
+
+	}
+
+
+	// TODO: implement the min query
+	int Fraction::min() const {
+
+		int lowest;
+
+		if (m_numer < m_denom) {
+
+			lowest = m_numer;
+
+		}
+		else {
+			lowest = m_denom;
+		}
+
+		return lowest;
+
+
+	}
 
 	// gcd returns the greatest common divisor of num and denom
-    //
-    int Fraction::gcd() const {
-        int mn = min();  // min of numerator and denominator
-        int mx = max();  // max of numerator and denominator
-        int g_c_d = 1;
-        bool found = false;
+	//
 
-        for (int x = mn; !found && x > 0; --x) { // from mn decrement until divisor found
-            if (mx % x == 0 && mn % x == 0) {
-                found = true;
-                g_c_d = x;
-            }
-        }
-        return g_c_d;
-    }
 
-    // TODO: implement the reduce modifier
+	int Fraction::gcd() const {
+		int mn = min();  // min of numerator and denominator
+		int mx = max();  // max of numerator and denominator
+		int g_c_d = 1;
+		bool found = false;
 
-    // TODO: implement the display query
+		for (int x = mn; !found && x > 0; --x) { // from mn decrement until divisor found
+			if (mx % x == 0 && mn % x == 0) {
+				found = true;
+				g_c_d = x;
+			}
+		}
+		return g_c_d;
+	}
 
-    // TODO: implement the isEmpty query
+	// TODO: implement the reduce modifier
 
-    // TODO: implement the + operator
+	void Fraction::reduce() { //divide numerator & denomator by return value of gcd() function
+
+		int divisor = gcd();
+		
+		m_numer /= divisor;
+		m_denom /= divisor;
+		
+
+
+	}
+
+	// TODO: implement the display query
+	
+	void Fraction::display() const {
+
+		if (!isEmpty()) {
+
+			if (m_denom == 1) {
+				cout << m_numer << endl;
+			}
+			else {
+
+				cout << m_numer << "/" << m_denom << endl;
+			}
+
+		}
+		else {
+			cout << "no fraction stored" << endl;
+		}
+
+
+	}
+
+
+	// TODO: implement the isEmpty query
+
+	bool Fraction::isEmpty() const {
+
+		bool Empty = false;
+
+		if (m_denom == 0) {
+			Empty = true;
+		}
+		
+		return Empty;
+
+	}
+
+	// TODO: implement the + operator
+
+	Fraction Fraction::operator+(const Fraction& rhs) const {
+
+
+		//Because safe empty state is 0/0, I dont need a an if statement to check.
+		// I may have an if statement for optimization purposes but im not sure if it'd make a big difference.
+		Fraction New;
+
+		
+		New.m_numer = (m_numer * rhs.m_denom) + (m_denom * rhs.m_numer);
+		New.m_denom = m_denom * m_numer;
+
+		New.reduce();
+
+		return New;
+
+	}
+
+}
--- Workshops/WS05/in-lab/Fraction.h
+++ Workshops/WS05/in-lab/Fraction.h
@@ -1,17 +1,52 @@
 // TODO: header file guard
 
-// TODO: create namespace
+#ifndef _SICT_FRACTION_H
+#define _SICT_FRACTION_H
 
+
+
+
+
+// TODO: create namespace
+namespace sict{
 	// TODO: define the Fraction class
 	class Fraction {
 		// TODO: declare instance variables 
 
+		int m_numer; //numerator 
+		int m_denom; //demonator
+
+
 		// TODO: declare private member functions
 
+
+
 	public:
+
 		// TODO: declare public member functions
 
+		Fraction();
+
+		Fraction(int numer, int denom) {
+
+		}
+
+		int max() const;
+		int min() const;
+
+		void reduce();
+		int gcd() const;
+		bool isEmpty() const;
+		void display() const;
+
 		// TODO: declare the + operator overload
+		Fraction operator+(const Fraction& rhs) const;
+
 
 	};
 
+		
+
+	}
+
+#endif
