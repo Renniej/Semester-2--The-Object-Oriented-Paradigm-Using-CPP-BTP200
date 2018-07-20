f900c36cd8e0e8fca195dfcadce6065deed91596
Renniej <tai.juan2@gmail.com>
2018-07-19 23:59:22 -0400

    ms2

---
+++ Workshops/WS08/in_lab/iAccount.cpp
@@ -0,0 +1,22 @@
+#include "iAccount.h"
+
+
+
+
+
+
+namespace sict {
+
+	iAccount * CreateAccount(const char * type, double start_balance) {
+
+		if (strcmp(type, "Savings") == 0) {
+
+			iAccount * = new 
+
+		}
+	
+	
+	}
+
+
+}
