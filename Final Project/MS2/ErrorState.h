f72b99f5db2c93b98825268a382636abb212020a
tai <tai.juan2@gmail.com>
2018-07-19 21:07:52 -0400

    ms2 progress

--- Final Project/MS2/ErrorState.h
+++ Final Project/MS2/ErrorState.h
@@ -1,1 +1,33 @@
-#pragma once
+#ifndef ERRORSTATE_H
+#define ERRORSTATE_H
+
+
+
+
+
+class ErrorState {
+
+	char *ptr;
+
+public :
+
+	explicit ErrorState(const char* errorMessage = nullptr);
+	
+	ErrorState(const ErrorState& em) = delete; //stops copy constructor from being called.
+	ErrorState& operator=(const ErrorState& em) = delete;
+	virutal ~ErrorState();
+	
+	void clear();
+	bool isClear() const;
+	void message(const char * str);
+	const char* message() const;
+
+
+
+};
+
+std::ostream& operator<<(const char* message, std::ostream& os);
+
+
+
+#endif
