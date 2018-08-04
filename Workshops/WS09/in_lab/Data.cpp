fb4da1a468a266755abed7e9a2142daefcb7122f
tai <tai.juan2@gmail.com>
2018-08-04 15:01:34 -0400

    WS09 done

--- Workshops/WS09/in_lab/Data.cpp
+++ Workshops/WS09/in_lab/Data.cpp
@@ -1,79 +1,24 @@
-
+#include <cmath>
+#include "Data.h"
 
 namespace sict {
 	// readRow extracts a single record from input and stores n data fields in data
 	// - includes error checking: checks if the name of the row is valid.
 	//
-	bool readRow(std::istream& input, const char* name, double* data, int n) {
-		char rowname[2000];
-		bool ok = !input.bad();
 
-		// row format is 
-		// name,1,2,3,4,5,6
 
-		if (ok) {
-			// read the name  first
 
-			input.get(rowname, 2000, ',');
-			// note: get will read a string upto the , character
-			// it will leave the , character in the input buffer 
 
-			// check that the caller is reading the correct row (name)
-			// - if the user is reading the wrong row, return an error
-			if (std::strcmp(rowname, name) != 0) {
-				// wrong row
-				std::cout << "Cannot parse row for " << name << std::endl;
-				input.ignore(2000, '\n');
-				ok = false;
-			}
-		}
+	
 
-		if (ok)
-			// read the data array
-			ok = read(input, data, n);
 
-		// finish the line
-		input.ignore(2000, '\n');
-		return ok;
-	}
-
-	bool readRow(std::istream& input, const char* name, int* data, int n) {
-		char rowname[2000];
-		bool ok = !input.bad();
-
-		// row format is 
-		// name,1,2,3,4,5,6
-
-		if (ok) {
-			// read the name  first
-
-			input.get(rowname, 2000, ',');
-			// note: get will read a string upto the , character
-			// it will leave the , character in the input buffer 
-
-			// check that the caller is reading the correct row (name)
-			// - if the user is reading the wrong row, return an error
-			if (std::strcmp(rowname, name) != 0) {
-				// wrong row
-				std::cout << "Cannot parse row for " << name << std::endl;
-				input.ignore(2000, '\n');
-				ok = false;
-			}
-		}
-
-		if (ok)
-			// read the data array
-			ok = read(input, data, n);
-
-		// finish the line
-		input.ignore(2000, '\n');
-		return ok;
-	}
 
 	// answers outputs statistics for visual validation of data
 	//
 	void answers(const int* year, const int* population, const int* violentCrime,
 		const double* violentCrimeRate, const int* grandTheftAuto, const int n) {
+
+
 
 		// Q1: print the population change in millions of people
 		// - print answer correct to 2 decimal places.
@@ -81,24 +26,57 @@ namespace sict {
 		// note that population is an integer, so dividing by 1000000 would yield "6"
 
 
+		if (population != nullptr && year != nullptr) {
+
+			
+			int Population_Change = population[0] - population[n - 1];
+
+			if (Population_Change < 0) {
+				Population_Change *= -1;
+			}
+			
+
+
+		
+
+			//Get num of digit
+		
+
+			
+			
+
+
+			std::cout << "Population change from " << year[0] << " to " <<  year[n-1] << " is " << std::setprecision(2) << std::fixed << (simplifyNum(Population_Change,  2)) << " million" << std::endl;
+
+		}
 
 
 
 		// Q2. print whether violent crime rate has gone up or down between 2000 and 2005
 
+	
 
-
-
+		
+		std::cout << "Violent Crime trend is " << (violentCrimeRate[0] < violentCrimeRate[n-1]? "up" : "down") << std::endl;
 
 
 		// Q3 print the GTA number accurate to 0 decimal places
 
 
+		std::cout << "There are " << std::setprecision(2) << std::fixed << (simplifyNum(average(grandTheftAuto, n), 1)) << " million Grand Theft Auto incidents on average a year" << std::endl;
 
 
 		// Q4. Print the min and max violentCrime rates
 
+		//Find max and min crime rates
+	
+
+		std::cout << "The Minimum Violent Crime rate was " << int(min(violentCrimeRate, n)) << std::endl;
+		std::cout << "The Maximum Violent Crime rate was "<< int(max(violentCrimeRate, n))<< std::endl;
 
 
 	}
+
+
+
 }
--- Workshops/WS09/in_lab/Data.h
+++ Workshops/WS09/in_lab/Data.h
@@ -1,6 +1,10 @@
 
+#include <string>
+#include<cstring>
+#include <iostream>
+#include <iomanip>
 
-
+using namespace std;
 
 namespace sict {
 	// These numbers define 
@@ -13,17 +17,156 @@ namespace sict {
 	const int SMALLEST_NUMBER = 0;          
 	const int COLUMN_WIDTH    = 15;
 
+
+	
 	// max returns the largest item in data
 	//
 
+	template<typename T>
+	double simplifyNum(T num, int dec_places) {
+
+		\
+
+		int Num_Of_Digits = num_of_digits(num);
+		return double(num)/ pow(10, Num_Of_Digits - dec_places);
+	}
+	
+	template<typename T> //gets num of digits;
+	int num_of_digits( T  base) {
+
+	
+
+		int Num_Of_Digits = 0;
+
+		for (int i = 0; base-1 >= 0; ++i) {
+			base /= 10;
+			Num_Of_Digits++;
+		}
+
+
+		return Num_Of_Digits;
+	}
+
+
+
+	template<typename T>
+	T max(const T* data, int n) {
+
+		T max = data[0];
+
+		for (int i = 0; i < n; ++i) {
+
+			if (data[i] > max) {
+
+				max = data[i];
+
+			}
+
+		}
+
+		return max;
+
+	}
+
 
 	// min returns the smallest item in data
-	//
+
+	template<typename T>
+	T min(const T* data, int n) {
+
+		T min = data[0];
+
+		for (int i = 0; i < n; ++i) {
+
+			if (data[i] < min) {
+
+				min = data[i];
+
+			}
+
+		}
+
+		return min;
+
+	}
 
         
 	// sum returns the sum of n items in data
 	//    
    
+	template<typename T>
+	T sum(const T* data, int n) {
+
+		T sum = 0;
+
+		for (int i = 0; i < n; ++i) {
+
+			sum += data[i];
+
+		}
+		//std::cout << "SUM: " << sum<<endl;
+		//std::cout << "N: " << n << endl << endl;
+		return sum;
+
+	}
+
+
+	template<typename T>
+	double average(const T* data, int n) {
+
+	
+		double avg = double(sum(data, n)) / double(n);
+
+		//std::cout << "AVG: " << avg << endl;
+		//std::cout << "N: " << n << endl << endl;
+
+		return avg;
+
+	}
+
+
+	template<typename T>
+	bool read(std::istream & input, T * data, int n)
+	{
+		
+
+		
+
+		for (int i = 0; i < n; ++i) {
+
+			input.get(); // gets delimter
+			input >> data[i];
+			
+		}
+
+		return input.bad() ? false : true;
+
+	}
+
+
+	template<typename T>
+	void display(const char * name, const T * data, int n){
+
+		
+
+		std::cout << std::setw(20) << std::right << name; // display name
+
+		for (int i = 0; i < n; ++i) {
+
+			
+			std::cout << std::setw(15) << std::right << data[i];
+
+		}
+
+		cout << endl; 
+		
+
+
+	}
+
+
+	
+	
 	// average returns the average of n items in data
 	//
 
@@ -36,8 +179,46 @@ namespace sict {
 	// display inserts n items of data into std::cout
 	//
 
-	bool readRow(std::istream& input, const char* name, int* data, int n);
-	bool readRow(std::istream& input, const char* name, double* data, int n);
+
+	template<typename T>
+	bool readRow(std::istream& input, const char* name, T* data, int n) {
+
+		
+		char rowname[2000];
+		bool ok = !input.bad();
+
+		// row format is 
+		// name,1,2,3,4,5,6
+
+		if (ok) {
+			// read the name  first
+
+			input.get(rowname, 2000, ',');
+			// note: get will read a string upto the , character
+			// it will leave the , character in the input buffer 
+
+			// check that the caller is reading the correct row (name)
+			// - if the user is reading the wrong row, return an error
+			if (std::strcmp(rowname, name) != 0) {
+				// wrong row
+				std::cout << "Cannot parse row for " << name << std::endl;
+				input.ignore(2000, '\n');
+				ok = false;
+			}
+		}
+
+		if (ok)
+			// read the data array
+			ok = read(input, data, n);
+
+		// finish the line
+		input.ignore(2000, '\n');
+		return ok;
+	}
+
 	void answers(const int*, const int*, const int*, const double*, const int*, const int n);
-}
+
+
+	
+	}
 
