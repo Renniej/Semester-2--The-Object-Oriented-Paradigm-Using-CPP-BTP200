ed2235a958ec208248115c305f8388ffc2eb9754
Renniej <tai.juan2@gmail.com>
2018-08-09 12:32:51 -0400

    MS4 finsihed

--- Final Project/MS5/iProduct.h
+++ Final Project/MS5/iProduct.h
@@ -8,25 +8,39 @@ using namespace std;
 namespace AMA {
 
 	class iProduct {
-
+		 
 	public:
 
+		virtual ~iProduct() 
+			{}
+
 		virtual std::fstream& store(std::fstream& file, bool newline = true) const = 0;
+
 		virtual std::fstream& load(std::fstream& file) = 0;
+
 		virtual std::ostream& write(std::ostream& os, bool linear)const = 0;
+
 		virtual std::istream& read(std::istream& is) = 0;
 
 		virtual bool operator==(const char* unit) const = 0;
-		virtual int operator+=(int) = 0;
-		virtual bool operator >(const iProduct&) const = 0;
-
-
 
 		virtual double total_cost() const = 0;
+
 		virtual const char* name() const = 0;
+
 		virtual void quantity(int num) = 0;
+
 		virtual int qtyNeeded() const = 0;
+
+
 		virtual int quantity() const = 0;
+
+		virtual int operator+=(int num) = 0;
+
+
+		virtual bool operator >(const iProduct&) const = 0;
+	
+		
 
 	};
 
